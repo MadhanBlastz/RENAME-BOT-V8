7fcd7d61a737b7c415709ab161cbd9c7b333fef6
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-08-20 19:53:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID = os.environ.get("API_ID", "12618934")  # ⚠️ Required
-    API_HASH = os.environ.get("API_HASH", "49aacd0bc2f8924add29fb02e20c8a16")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6728153756:AAFQdlTJhROtUrtasgnK9E3EPEZrjWBraCU")  # ⚠️ Required
+    API_ID = os.environ.get("API_ID", "21661450")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "79612bc71908f95372808520a7eeee74")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7463606692:AAFVFmyJ8YdI4ZhsY9jhnVQrMOYZzWM1BNI")  # ⚠️ Required
 
     # premium 4g renaming client
     STRING_API_ID = os.environ.get("STRING_API_ID", "")
@@ -24,10 +24,10 @@ class Config(object):
     BOT_UPTIME = time.time()
     START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/1e9f0748931102b81caa3.jpg")
     ADMIN = [int(admin) if id_pattern.search(
-        admin) else admin for admin in os.environ.get('ADMIN', '1297128957').split()]  # ⚠️ Required
+        admin) else admin for admin in os.environ.get('ADMIN', '2021408974').split()]  # ⚠️ Required
     
     FORCE_SUB = os.environ.get("FORCE_SUB", "OTT_ARAKAL_THERAVAD_MOVIESS") # ⚠️ Required Username without @
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002116152617"))  # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002233706633"))  # ⚠️ Required
     FLOOD = int(os.environ.get("FLOOD", '10'))
     BANNED_USERS = set(int(x) for x in os.environ.get(
         "BANNED_USERS", "1234567890").split())
